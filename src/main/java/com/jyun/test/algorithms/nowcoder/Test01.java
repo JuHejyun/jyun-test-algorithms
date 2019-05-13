814638b53cbdbd207e8037f2ec9b071192f0216c
zhuhejun <zhuhejun@jd.com>
2019-05-13 10:57:31 +0800

    牛客

---
+++ src/main/java/com/jyun/test/algorithms/nowcoder/Test01.java
@@ -0,0 +1,36 @@
+package com.jyun.test.algorithms.nowcoder;
+
+import org.junit.Test;
+
+public class Test01 {
+
+
+	@Test
+	public void testReplaceSpace(){
+		String s = replaceSpace(new StringBuffer("a b n   c"));
+		System.out.println(s);
+	}
+
+
+
+	public String replaceSpace(StringBuffer str) {
+		int P1 = str.length() - 1;
+		for (int i = 0; i <= P1; i++)
+			if (str.charAt(i) == ' ')
+				str.append("  ");
+
+		int P2 = str.length() - 1;
+		while (P1 >= 0 && P2 > P1) {
+			char c = str.charAt(P1--);
+			if (c == ' ') {
+				str.setCharAt(P2--, '0');
+				str.setCharAt(P2--, '2');
+				str.setCharAt(P2--, '%');
+			} else {
+				str.setCharAt(P2--, c);
+			}
+		}
+		return str.toString();
+	}
+
+}
